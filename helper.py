ef494abd9bdda988eff697f910a27e2e96bd7ff9
jhzhu <jhzhu@telenavsoftware.com>
2024-07-02 17:43:46 +0800

    MTCNN commit

---
+++ helper.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+if __name__ == '__main__':
+    # 测试代码
+    H, W = 1080, 1920
+    # print()
+    # print((W - 12) // 2 + 1)
+    print(((H - 12) // 2 + 1) * (W - 12) // 2 + 1)
